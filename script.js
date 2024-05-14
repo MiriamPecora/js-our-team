1238da46108dcf64474886905cc3ce73cef1f6f4
MiriamPecora <miriampecora97@gmail.com>
2024-05-14 19:04:07 +0200

    [UPD] Printed the array in the DOM by creating elements in js file

--- script.js
+++ script.js
@@ -33,4 +33,34 @@ const team = [
 ];
 
 // Stampo in console l'array di oggetti
-console.log(team);
+// console.log(team);
+
+
+
+for (let i = 0; i < team.length; i++) {
+    const currentObj = team[i];
+    // console.log(currentObj);
+
+    const ul = document.getElementById("list");
+
+    // Stampo nome in pagina
+    let li = document.createElement("li");
+    let h3 = document.createElement("h3");
+
+    h3.append(currentObj.name);
+    li.append(h3);
+
+    // Stampo ruolo in pagina
+    let p = document.createElement("p");
+
+    p.append(currentObj.role);
+    li.append(p);
+
+    // Stampo le stringhe delle foto
+    let imageP = document.createElement("p");
+
+    imageP.append(currentObj.image);
+    li.append(imageP);
+
+    ul.append(li);
+}
